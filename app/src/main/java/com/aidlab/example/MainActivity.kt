f3c7552f37d70ecff8bc3232732c8a9290f9cd67
Miłosz Głowaczewski <milosz.glowaczewski@aidlab.com>
2021-03-02 11:21:50 +0100

    Updated project to AndroidX, changed target sdk version to 30

--- app/src/main/java/com/aidlab/example/MainActivity.kt
+++ app/src/main/java/com/aidlab/example/MainActivity.kt
@@ -14,7 +14,7 @@ import com.aidlab.sdk.communication.*
 
 import android.content.Intent
 import android.os.Handler
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 
