ef400948340c7b9fce38162f0ad07bcf478dccd7
Jakub Domaszewicz <jdomaszewicz@aidlab.com>
2026-03-01 22:03:19 +0100

    chore: tighten linting rules

--- lint.sh
+++ lint.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_DIR}"
 
-./gradlew :app:ktlintCheck :app:detekt :app:lintDebug
+./gradlew :app:ktlintCheck :app:detekt :app:lintDebug :app:lintRelease
